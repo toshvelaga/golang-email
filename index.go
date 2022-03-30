d000b3522026efbf59ca120a8195ebab5ba0f171
Tosh Velaga <toshvelaga@gmail.com>
2022-03-30 16:51:48 -0600

    first commit

---
+++ index.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	// load .env file from given path
+	// we keep it empty it will load .env from current directory
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Sender data.
+	from := "ohmystreamer@gmail.com"
+	password := os.Getenv("EMAIL_PASSWORD")
+
+	// Receiver email address.
+	to := []string{
+		"toshvelaga@gmail.com",
+	}
+
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	// Message.
+	message := []byte(
+		"Subject: This is a test email\r\n" + "\r\n" + "Here is a test message\r\n")
+
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Sending email.
+	emailErr := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if emailErr != nil {
+		fmt.Println(emailErr)
+		return
+	}
+	fmt.Printf("Email successfully sent to %s", to[0])
+}
